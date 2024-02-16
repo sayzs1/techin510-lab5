7d3a549fe77ea23f866de89afd7d1e495b6c0e5c
sayzs1@yeah.net <sayzs1@yeah.net>
2024-02-15 23:11:00 -0800

    update

--- scraper.py
+++ scraper.py
@@ -11,7 +11,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 db_user = os.getenv('DB_USER')
 db_pw = os.getenv('DB_PASSWORD')
 db_host = os.getenv('DB_HOST')
@@ -24,68 +23,25 @@ def get_db_conn():
     conn.autocommit = True
     return conn
 
-
 URL = 'https://visitseattle.org/events/page/'
 URL_LIST_FILE = './data/links.json'
 URL_DETAIL_FILE = './data/data.json'
 
 def list_links():
-    res = requests.get(URL + '1/')
+    res = requests.get(f'{URL}1/')
     last_page_no = int(re.findall(r'bpn-last-page-link"><a href=".+?/page/(\d+?)/.+" title="Navigate to last page">', res.text)[0])
 
     links = []
     for page_no in range(1, last_page_no + 1):
-        res = requests.get(URL + str(page_no) + '/')
+        res = requests.get(f'{URL}{page_no}/')
         links.extend(re.findall(r'<h3 class="event-title"><a href="(https://visitseattle.org/events/.+?/)" title=".+?">.+?</a></h3>', res.text))
 
     json.dump(links, open(URL_LIST_FILE, 'w'))
 
-def fetch_geolocation(location_name):
-    """Fetch geolocation (latitude and longitude) for a given location name."""
-    base_url = "https://nominatim.openstreetmap.org/search"
-    params = {
-        'q': location_name,
-        'format': 'json',
-        'limit': 1
-    }
-    headers = {
-        'User-Agent': 'EventScraperBot 1.0'  # Change this to a more descriptive user-agent for your application
-    }
-    response = requests.get(base_url, params=params, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        if data:
-            return {
-                'latitude': data[0]['lat'],
-                'longitude': data[0]['lon']
-            }
-    return None
-
-def fetch_weather(latitude, longitude):
-    """Fetch weather data for the given latitude and longitude from NWS."""
-    gridpoint_url = f"https://api.weather.gov/points/{latitude},{longitude}"
-    response = requests.get(gridpoint_url)
-    if response.status_code == 200:
-        gridpoint_data = response.json()
-        forecast_url = gridpoint_data['properties']['forecast']
-
-        forecast_response = requests.get(forecast_url)
-        if forecast_response.status_code == 200:
-            forecast_data = forecast_response.json()
-            period = forecast_data['properties']['periods'][0]
-            weather = {
-                'condition': period['shortForecast'],
-                'temperature_min': None,  
-                'temperature_max': period['temperature'],
-                'wind_chill': None
-            }
-            return weather
-    return None    
- 
-
 def get_detail_page():
     links = json.load(open(URL_LIST_FILE, 'r'))
     data = []
+    err = []
     for link in links:
         try:
             row = {}
@@ -93,65 +49,107 @@ def get_detail_page():
             row['title'] = html.unescape(re.findall(r'<h1 class="page-title" itemprop="headline">(.+?)</h1>', res.text)[0])
             datetime_venue = re.findall(r'<h4><span>.*?(\d{1,2}/\d{1,2}/\d{4})</span> \| <span>(.+?)</span></h4>', res.text)[0]
             row['date'] = datetime.datetime.strptime(datetime_venue[0], '%m/%d/%Y').replace(tzinfo=ZoneInfo('America/Los_Angeles')).isoformat()
-            row['venue'] = datetime_venue[1].strip() # remove leading/trailing whitespaces
+            row['venue'] = datetime_venue[1].strip()  # remove leading/trailing whitespaces
             metas = re.findall(r'<a href=".+?" class="button big medium black category">(.+?)</a>', res.text)
             row['category'] = html.unescape(metas[0])
             row['location'] = metas[1]
-        
-            # Fetch geolocation data
-            geolocation = fetch_geolocation(row['location'])
-            if geolocation:
-                row['latitude'] = geolocation['latitude']
-                row['longitude'] = geolocation['longitude']
-            else:
-                row['latitude'] = None
-                row['longitude'] = None
-        
-            if geolocation:
-                row['latitude'] = geolocation['latitude']
-                row['longitude'] = geolocation['longitude']
-        
-                weather = fetch_weather(row['latitude'], row['longitude'])
-                if weather:
-                    row.update(weather)
-                else:
-                    row['weather'] = 'Not available'
-
             data.append(row)
         except IndexError as e:
+            err.append(link)
             print(f'Error: {e}')
             print(f'Link: {link}')
+    if err:
+        links = [l for l in links if l not in err]
+        json.dump(links, open(URL_LIST_FILE, 'w'))
     json.dump(data, open(URL_DETAIL_FILE, 'w'))
 
+def get_geo_weather():
+    urls = json.load(open(URL_LIST_FILE, 'r'))
+    data = json.load(open(URL_DETAIL_FILE, 'r'))
+    err_url = []
+    err_data = []
+    for url, row in zip(urls, data):
+        res = requests.get(f"https://nominatim.openstreetmap.org/search.php?q={row['location']} seattle&format=jsonv2")
+        if not res.json():
+            res = requests.get(f"https://nominatim.openstreetmap.org/search.php?q={row['location']}&format=jsonv2")
+        location = res.json()
+        if not location:
+            print(f'Location not found: {url}')
+            err_url.append(url)
+            err_data.append(row.copy())
+            continue
+        lat, lon = location[0]['lat'], location[0]['lon']
+        row['lat'] = lat
+        row['lon'] = lon
+        res = requests.get(f"https://api.weather.gov/points/{lat},{lon}")
+        weather_point = res.json()
+        try:
+            forecast_url = weather_point['properties']['forecast']
+            forecast_url_grid = weather_point['properties']['forecastGridData']
+            res = requests.get(forecast_url)
+            row['condition'] = res.json()['properties']['periods'][0]['shortForecast']
+            res = requests.get(forecast_url_grid)
+            row["minTemperature"] = res.json()['properties']["minTemperature"]["values"][0]["value"]
+            row["maxTemperature"] = res.json()['properties']["maxTemperature"]["values"][0]["value"]
+            row["windChill"] = res.json()['properties']["windChill"]["values"][0]["value"]
+        except Exception as e:
+            print(f'Weather not found: {url}, {row["venue"]}')
+            print(f'Error: {e}')
+            err_url.append(url)
+            err_data.append(row.copy())
+            # continue
+    if err_url:
+        urls = [u for u in urls if u not in err_url]
+        data = [d for d in data if d not in err_data]
+        json.dump(urls, open(URL_LIST_FILE, 'w'), indent=2)
+    json.dump(data, open(URL_DETAIL_FILE, 'w'), indent=2)
+
 def insert_to_pg():
+    q = '''
+    CREATE TABLE IF NOT EXISTS events (
+        url TEXT PRIMARY KEY,
+        title TEXT,
+        date TIMESTAMP WITH TIME ZONE,
+        venue TEXT,
+        category TEXT,
+        location TEXT,
+        latitude FLOAT,
+        longitude FLOAT,
+        condition TEXT,
+        mintemperature FLOAT,
+        maxtemperature FLOAT,
+        windchill FLOAT
+    );
+    '''
     conn = get_db_conn()
     cur = conn.cursor()
-    
+    cur.execute(q)
+
     urls = json.load(open(URL_LIST_FILE, 'r'))
     data = json.load(open(URL_DETAIL_FILE, 'r'))
     for url, row in zip(urls, data):
-        q_insert = '''
-        INSERT INTO events (url, title, date, venue, category, location, weather_condition, temperature_max, temperature_min, wind_chill)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        q = '''
+        INSERT INTO events (url, title, date, venue, category, location, latitude, longitude, condition, mintemperature, maxtemperature, windchill)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         ON CONFLICT (url) DO NOTHING;
         '''
-        cur.execute(q_insert, (
-            url, 
-            row['title'], 
-            row['date'], 
-            row['venue'], 
-            row['category'], 
+        cur.execute(q, (
+            url,
+            row['title'],
+            row['date'],
+            row['venue'],
+            row['category'],
             row['location'],
-            row.get('condition'),  
-            row.get('temperature_max'),
-            row.get('temperature_min'),
-            row.get('wind_chill')
+            row['lat'],
+            row['lon'],
+            row['condition'],
+            row['minTemperature'],
+            row['maxTemperature'],
+            row['windChill']
         ))
-    conn.commit()
-    cur.close()
-    conn.close()
-    
+
 if __name__ == '__main__':
     list_links()
     get_detail_page()
+    get_geo_weather()
     insert_to_pg()
